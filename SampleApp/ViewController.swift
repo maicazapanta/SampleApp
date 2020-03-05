e1389e63835c7d31bcd536dc2abbaec6e2dda52a
maicazapanta <maicazapanta@github.com>
2020-03-05 17:20:28 +0800

    Initial Commit

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Maica Zapanta on 3/5/20.
+//  Copyright © 2020 Maica Zapanta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
