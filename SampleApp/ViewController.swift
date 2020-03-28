d6f2ae88177037aa1ebe88f2befdd38fa216b4cf
maicazapanta <maicazapanta@github.com>
2020-03-28 13:20:20 +0800

    Added pod file for alamofire

--- SampleApp/ViewController.swift
+++ SampleApp/ViewController.swift
@@ -7,13 +7,30 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        NetworkingService.shared.getCoffees {
+            print("GOT IT")
+        }
     }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 
 
 }
